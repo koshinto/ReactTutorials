4bcf2d9bab92c265d68f6ab5c58744b5ac26c56f
Kosuke Shinto <kou.shinto@icloud.com>
2021-03-03 21:26:36 +0900

    Table Rows

--- src/Mock.tsx
+++ src/Mock.tsx
@@ -1,20 +1,58 @@
 import React from 'react'
 
-const products = [
-  {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
-  {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
-  {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
-  {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
-  {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
-  {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
-]
+interface ProductRow {
+  category: string,
+  price: string,
+  stocked: boolean,
+  name: string
+}
 
-function ProductRow() {}
+function ProductRow(props: ProductRow) {
+  const name = props.stocked ?
+    props.name :
+    <span style={{color: 'red'}}>{props.name}</span>
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{props.price}</td>
+    </tr>
+  )
+}
 
-function ProductCatagoryRow() {}
+interface CategoryName {
+  category: string
+}
 
-function ProductTable() {}
+function ProductCatagoryRow(props: CategoryName) {
+  return (
+    <td colSpan={2}>
+      {props.category}
+    </td>
+  )
+}
 
-function SearchBar() {}
+interface CategoryRows {
+  products: ProductRow[]
+}
 
-export default function FilterableProductTable() {}
+function ProductTable(props: CategoryRows) {
+  return 
+}
+
+function SearchBar() {
+  return <input></input>
+}
+
+export default function FilterableProductTable() {
+  const products = [
+    {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
+    {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
+    {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
+    {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
+    {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
+    {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
+  ]
+  return (
+    <div></div>
+  )
+}
